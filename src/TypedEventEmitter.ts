8efb8a1023af1cfb7deb9064ff70ee364ccebc78
mustib <mmi197here@gmail.com>
2024-05-30 13:59:02 +0300

    Add TypedEventEmitter

---
+++ src/TypedEventEmitter.ts
@@ -0,0 +1,33 @@
+import type { EventEmitter } from 'node:events';
+
+import type { Func } from 'types.js';
+
+export type TypedEventEmitter<T extends Record<string, any>> = {
+  emit<Name extends keyof T, Args extends T[Name]>(
+    eventName: Name,
+    ...args: Args extends any[] ? Args : [Args]
+  ): boolean;
+} & {
+  [key in
+  | 'on'
+  | 'once'
+  | 'addListener'
+  | 'prependListener'
+  | 'prependOnceListener']: <Name extends keyof T, Args extends T[Name]>(
+    eventName: Name,
+    listener: (...args: Args extends any[] ? Args : [Args]) => void
+  ) => TypedEventEmitter<T>;
+} & {
+  [key in 'removeListener' | 'off']: <Name extends keyof T>(
+    eventName: Name,
+    ...args: any[]
+  ) => TypedEventEmitter<T>;
+} & {
+  removeAllListeners(eventName: keyof T): TypedEventEmitter<T>;
+  setMaxListeners(number: number): TypedEventEmitter<T>;
+  listenerCount(eventName: keyof T): number;
+  listeners(eventName: keyof T): Func[];
+  rawListeners(eventName: keyof T): Func[];
+  eventNames: EventEmitter['eventNames'];
+  getMaxListeners: EventEmitter['getMaxListeners'];
+};
