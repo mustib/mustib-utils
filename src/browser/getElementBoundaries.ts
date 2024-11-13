14b6ee38396506a29405594afd8bc75bc45bb6ba
mustib <mmi197here@gmail.com>
2024-11-13 09:39:15 +0200

    Add getElementBoundaries

---
+++ src/browser/getElementBoundaries.ts
@@ -0,0 +1,55 @@
+export function getElementBoundaries(element: HTMLElement) {
+  const { innerHeight, innerWidth } = window;
+
+  const {
+    top: elementTop,
+    bottom,
+    left: elementLeft,
+    right: elementRight,
+    width,
+    height,
+  } = element.getBoundingClientRect();
+
+  const elementBottom = innerHeight - bottom;
+
+  const isTopInPage = elementTop >= 0 && elementTop <= innerHeight;
+  const isBottomInPage = elementBottom >= 0 && elementBottom <= innerHeight;
+  const isLeftInPage = elementLeft >= 0 && elementLeft <= innerWidth;
+  const isRightInPage = elementRight >= 0 && elementRight <= innerWidth;
+
+  const isTopVisible = isTopInPage && (isLeftInPage || isRightInPage);
+  const isTopFullyVisible = isTopInPage && isLeftInPage && isRightInPage;
+
+  const isBottomVisible = isBottomInPage && (isLeftInPage || isRightInPage);
+  const isBottomFullyVisible = isBottomInPage && isLeftInPage && isRightInPage;
+
+  const isLeftVisible = isLeftInPage && (isTopInPage || isBottomInPage);
+  const isLeftFullyVisible = isLeftInPage && isTopInPage && isBottomInPage;
+
+  const isRightVisible = isRightInPage && (isTopInPage || isBottomInPage);
+  const isRightFullyVisible = isRightInPage && isTopInPage && isBottomInPage;
+
+  const isFullyVisible =
+    isTopFullyVisible &&
+    isBottomFullyVisible &&
+    isLeftFullyVisible &&
+    isRightFullyVisible;
+
+  return {
+    elementTop,
+    elementBottom,
+    elementLeft,
+    elementRight,
+    width,
+    height,
+    isTopVisible,
+    isTopFullyVisible,
+    isBottomVisible,
+    isBottomFullyVisible,
+    isLeftVisible,
+    isLeftFullyVisible,
+    isRightVisible,
+    isRightFullyVisible,
+    isFullyVisible,
+  };
+}
