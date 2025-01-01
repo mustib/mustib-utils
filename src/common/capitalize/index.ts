d824750dc472b96c34d7a4c0766c07893f87ac31
mustib <mmi197here@gmail.com>
2025-01-01 19:07:26 +0200

    Add capitalize docs

--- src/common/capitalize/index.ts
+++ src/common/capitalize/index.ts
@@ -1,3 +1,21 @@
+type Options = {
+  /**
+   * Whether to capitalize only the first word.
+   * @default false
+   */
+  onlyFirstWord?: boolean;
+  /**
+   * The delimiter to split the string into words.
+   * @default ' '
+   */
+  splitter?: string;
+  /**
+   * The delimiter to join the capitalized words.
+   * @default splitter
+   */
+  joiner?: string;
+};
+
 const capitalizeFirst = (str: string) =>
   str[0].toUpperCase().concat(str.slice(1).toLowerCase());
 
@@ -11,13 +29,16 @@ const capitalizeFirst = (str: string) =>
  * @param {string} [options.joiner=options.splitter] - The delimiter to join the capitalized words (default: options.splitter).
  * @returns {string} The capitalized string.
  */
-export function capitalize(
-  str: string,
-  options?: { onlyFirstWord?: boolean; splitter?: string; joiner?: string }
-) {
-  const { onlyFirstWord: onlyFirst = false, splitter = ' ', joiner = splitter } = options || {};
+export function capitalize(str: string, options?: Options) {
+  const {
+    onlyFirstWord: onlyFirst = false,
+    splitter = ' ',
+    joiner = splitter,
+  } = options || {};
 
   if (typeof str !== 'string' || str === '') return str;
 
-  return (onlyFirst ? [str] : str.split(splitter)).map(capitalizeFirst).join(joiner);
+  return (onlyFirst ? [str] : str.split(splitter))
+    .map(capitalizeFirst)
+    .join(joiner);
 }
