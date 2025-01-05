4d77fe480a31e049d923ca72bc2a975957832932
mustib <mmi197here@gmail.com>
2025-01-05 12:48:50 +0200

    Fix docs getUtilityUrl version number

--- docs/src/content/docs/v2/utilities/getUtilityUrl.ts
+++ docs/src/content/docs/v2/utilities/getUtilityUrl.ts
@@ -18,5 +18,5 @@ export function getUtilityUrl<Category extends keyof Categories>(
   utility: Categories[Category],
   id?: string,
 ) {
-  return `/mustib-utils/v1/utilities/${category}/${utility}/${id ? `#${id}` : ''}`;
+  return `/mustib-utils/v2/utilities/${category}/${utility}/${id ? `#${id}` : ''}`;
 }
